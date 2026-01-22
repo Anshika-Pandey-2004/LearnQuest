11371c5c5904f71b023dcc7fa6640dad9f3acef7
Anshika-Pandey-2004 <anshikapandey1190104@gmail.com>
2026-01-22 20:18:13 +0530

    Fix: Remove unused imports and variables from App.js to resolve Vercel build errors

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar"
 import OpenRoute from "./components/core/Auth/OpenRoute"
-import { useNavigate } from "react-router-dom";
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
-import ContactUsForm from "./components/ContactPage/ContactUsForm";
 import ContactPage from "./pages/ContactPage";
 import MyProfile from "./components/core/Dashboard/MyProfile";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/core/Auth/PrivateRoute";
 import Error from "./pages/Error"
 import Settings from "./components/core/Dashboard/Settings";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
 import Cart from "./components/core/Dashboard/Cart";
 import { ACCOUNT_TYPE } from "./utils/constants";
@@ -32,12 +30,9 @@ import VideoDetails from "./components/core/ViewCourse/VideoDetails";
 import Instructor from "./components/core/Dashboard/Instructor"
 import toast from "react-hot-toast";
 import picture from '../src/assets/Images/Photo.png'
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-function App() {
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate(); 
+function App() { 
   
   const { user } = useSelector((state) => state.profile)
   const [toastStatus, settoastStatus] = useState(true)
